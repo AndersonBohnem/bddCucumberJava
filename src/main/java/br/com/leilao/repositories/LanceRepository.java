4aba7eb1edecb3c594c738e98b25236be23b76b6
Anderson Bohnemberger <bohnembergeranderson@gmail.com>
2024-10-26 15:44:43 -0300

    Subindo projeto onde foram realizados testes de aceitação em java utilizando BDD e a linguagem Gherkin para criar requisitos executáveis

---
+++ src/main/java/br/com/leilao/repositories/LanceRepository.java
@@ -0,0 +1,10 @@
+package br.com.leilao.repositories;
+
+import br.com.leilao.model.Lance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LanceRepository extends JpaRepository<Lance, Long> {
+
+}
---
+++ src/main/java/br/com/leilao/repositories/LeilaoRepository.java
@@ -0,0 +1,11 @@
+package br.com.leilao.repositories;
+
+import br.com.leilao.model.Leilao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LeilaoRepository extends JpaRepository<Leilao, Long>  {
+
+
+}
